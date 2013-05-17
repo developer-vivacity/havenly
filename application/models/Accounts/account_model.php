44310b88bbeee1ba359b6cd4ed366c5f55cf65c5
asheesh <kbs.php@gmail.com>
2013-05-17 07:25:26 +0530

    Add files and folders

--- application/models/Accounts/account_model.php
+++ application/models/Accounts/account_model.php
@@ -6,24 +6,35 @@ class Account_model extends CI_Model
    {
    parent::__construct();
    }
+   
+  // ---For create account table--------
    function creat_table()
    {
 	      $query=$this->db->query('CREATE TABLE IF NOT EXISTS account (
             id INT( 50 ) NOT NULL AUTO_INCREMENT ,
             username VARCHAR( 50 ) NOT NULL ,
             email VARCHAR( 100 ) NOT NULL ,
-            password VARCHAR( 255 ) NOT NULL ,
+            password VARCHAR( 255 ) NOT NULL ,Phone_number varchar(50) NOT NULL,Zip_code varchar(50) NOT NULL,
             PRIMARY KEY ( id )
            ) ENGINE = InnoDB');
    }
+   // For inser user informetion---------
    function insert_user_info($data,$email)
    {
+	   
 	 
 	    $this->db->where("email",$email);
 	    $query=$this->db->get("account");
        if($query->num_rows()==0)
        {
-        $this->db->insert('account',$data);
+         
+         $this->db->insert('account',$data);
+         foreach($data as $rows)
+         {
+		 $newdata =array('user_id'=>$rows->id,'user_name'=> $rows->username,'user_email'=> $rows->email,'Phone_number'=>$rows->Phone_number,'Zip_code'=>$rows->Zip_code);   
+	     } 
+	     $this->session->set_userdata($newdata);
+       
         return mysql_insert_id();
        }
 	   else
@@ -32,34 +43,33 @@ class Account_model extends CI_Model
 	   }
 		   
    }
-
+//---For user login and create session.......
    public function user_login($email,$password)
    {
  
- //echo $email."-----".$password;
+ 
    $this->db->where("email",$email);
    $this->db->where("password",$password);
    $query=$this->db->get("account");
-   //echo '<br>';
-  //echo $query->num_rows();
-  //die($query->num_rows());
+  
   
    if($query->num_rows()>0)
    {
     foreach($query->result() as $rows)
     {
       $newdata=$rows->username;
-      //$newdata =array('user_id'=>$rows->id,'user_name'=> $rows->username,'user_email'=> $rows->email,'logged_in'=> TRUE,'isAdmin'=>$rows->user_type,'Active_user'=>$rows->Active_account);
+      $newdata =array('user_id'=>$rows->id,'user_name'=> $rows->username,'user_email'=> $rows->email,'Phone_number'=>$rows->Phone_number,'Zip_code'=>$rows->Zip_code);
     }
-    //$this->session->set_userdata($newdata);
+    $this->session->set_userdata($newdata);
     
     return $newdata;
    }
    else
    return "";
-  //return "";
+  
  
    }
+   //----For check user mail id.......
    public function check_mail($email)
    {
 	   
@@ -76,6 +86,18 @@ class Account_model extends CI_Model
 		
 	 }     
    }
+   
+   // For update user informetion
+   public function update_user_info($data,$id)
+   {
+	   
+
+       $this->db->where("id",$id);
+       
+       $this->db->update("account", $data); 
+	 
+   }
+   // For update Password
    public function update_password($email,$password)
    {
 	   $data = array('password' => $password);
@@ -85,17 +107,19 @@ class Account_model extends CI_Model
        $this->db->update("account", $data); 
 	 
    }
+   // For count total rows
  public function total_rows()
  {
 
   $query=$this->db->query('SELECT * FROM account');
   return $query->num_rows();
  }
- function user_getall()
+ // For fetch user details which login.
+ function user_getall($id)
   {
-       $this->load->database();    
-       $query=$this->db->get('account');//user is a table in the database
-       return $query->result();
+           $query=$this->db->query("SELECT * FROM account where id=".$id."");
+    
+          return $query->result();
   }
 
 }

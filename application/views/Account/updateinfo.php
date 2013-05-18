cda83aee80d6cb72486c603e2afcc59d6d82adc8
asheesh <kbs.php@gmail.com>
2013-05-18 18:43:24 +0530

    Delete unwanted file/folder

--- application/views/Account/updateinfo.php
+++
@@ -1,5 +0,0 @@
-<div>
-Your Update password informetion has been send to your email account.<br/>
-Please go to your email account and get information account Password.
-<a href="http://www.havenly.com/testsite/index.php/Account">login User</a>
-</div>

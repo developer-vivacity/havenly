d46d0f8c33cb4ad96cf291284464e3412c2d9302
asheesh <kbs.php@gmail.com>
2013-05-24 06:00:32 +0530

    File Updated - accountconfirmation.php

--- application/views/Users/accountconfirmation.php
+++ application/views/Users/accountconfirmation.php
@@ -1,7 +1,8 @@
 <?php
 echo $accountinfo;
-echo '<a href="'.base_url().'index.php/Users/site/logout/">Log Out</a>';
+echo '<p><a href="'.base_url().'index.php/Users/site/login/">Edit</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="'.base_url().'index.php/Users/site/logout/">Log Out</a></p>';
 ?>
+
 
 
 

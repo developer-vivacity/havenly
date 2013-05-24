94ed7a7c9fb1a23dd7cdda10fb3274e6e0274443
asheesh <kbs.php@gmail.com>
2013-05-24 06:04:25 +0530

    File Updated - forgotpassword.php

--- application/views/Users/forgotpassword.php
+++ application/views/Users/forgotpassword.php
@@ -1,15 +1,15 @@
 <div>
 	<div class="signin_form">
 <p><h1>Forgot Password:</h1></p>
-   <?php 
+  <?php 
 	 if(!empty($register_info))
     echo $register_info;
     echo validation_errors('<p class="error">');
-   ?>
-   <?php echo form_open('Users/site/validatemail/');?>
-   <?php echo form_open('Users/site/validatemail/');?>
-  <label for="email">Enter Email:</label>
-  <input type="text" id="enteremail" name="enteremail"  />
+  ?>
+  <?php echo form_open('Users/site/validatemail/');?>
+
+ <label for="email">Enter Email:</label>
+ <input type="text" id="enteremail" name="enteremail" value="<?php if(isset($_POST['enteremail'])) echo $_POST['enteremail']; ?>"/>
  <input type="submit" class="" value="Sign in" />
  <?php echo form_close(); ?>
   <?php 

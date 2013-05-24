6ca473fee729feff768f452a5aa336cc530d1b28
asheesh <kbs.php@gmail.com>
2013-05-24 06:05:30 +0530

    File Updated - accountconfirmation.php

--- application/views/Users/accountconfirmation.php
+++ application/views/Users/accountconfirmation.php
@@ -1,8 +1,7 @@
 <?php
 echo $accountinfo;
-echo '<p><a href="'.base_url().'index.php/Users/site/login/">Edit</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="'.base_url().'index.php/Users/site/logout/">Log Out</a></p>';
+echo '<p><a href="'.base_url().'index.php/Users/site/UserEditInformation/">Edit</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="'.base_url().'index.php/Users/site/logout/">Log Out</a></p>';
 ?>
-
 
 
 

90302e63b3c1c7d8d67acae48351dfa97bdf318c
neha <neha@kindlebit-desktop.(none)>
2013-08-17 17:56:39 +0530

    File Add

---
+++ application/controllers/Concept/site.php
@@ -0,0 +1,32 @@
+<?php
+Class Site extends CI_Controller
+{
+    function __construct()
+    {
+      parent::__construct();
+   
+      $this->load->library('s3');
+      $this->load->library('session');
+      $this->load->model('concept_model');
+   }
+   function initial_concepts_for_user()
+   {
+	
+          $data['conceptboard']=$this->concept_model->initial_concepts_for_user();
+	 $this->load->view('Users/conceptboard',$data);
+   }
+   function  save_comment_concept_bord()
+   {
+	
+	if($_POST)
+	{
+		$this->concept_model->save_comment($_POST['holdcomment'],$_POST['holdconceptid'],$_POST['holdroomid']);
+		$this->initial_concepts_for_user();
+		redirect('/Concept/site/initial_concepts_for_user', 'refresh');
+	}   
+	   
+   }
+
+}
+
+?>

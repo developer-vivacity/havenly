8badd34e7b823840ec8f141c4c076c4d5395b881
Ankit087 <ankit.kumar@kindlebit.com>
2013-09-14 05:02:27 +0530

    Merged

--- application/libraries/Template.php
+++
@@ -1,9 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-   class Template
-   {
-      var $ci;
-      function __construct()
-      {
-         $this->ci =& get_instance();
-      }
-   }

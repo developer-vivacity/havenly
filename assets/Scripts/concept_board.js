2f149056d45d9fd652c91369d9c9ffea52ab5ab6
Ankit087 <ankit.kumar@kindlebit.com>
2013-09-14 06:56:49 +0530

    File Updated

---
+++ assets/Scripts/concept_board.js
@@ -0,0 +1,76 @@
+$(function(){
+		var btnUpload=$('#imageUpload');
+		var mestatus=$('#mestatus');
+		var files=$('#files');
+
+		new AjaxUpload(btnUpload, {
+		action: $("#siteurl").val()+'index.php/Admin/site/upload_image_for_concept_board/'+'imageUpload'+'/'+$("#userroomid").val(),
+		name: 'imageUpload',
+		onSubmit: function(file, ext){
+		    
+		      if (! (ext && /^(jpg|png|jpeg|gif)$/.test(ext)))
+		      { 
+		      mestatus.text('Only JPG, PNG or GIF files are allowed');
+		      return false;
+		      }
+		        mestatus.html('<img src="'+$("#siteurl").val()+'assets/Images/ajax-loader.gif" height="16" width="16">');
+		  },
+			onComplete: function(file, response)
+			{
+				 
+				
+				  var obj=$.parseJSON(response);
+				
+			           if(obj.success==="success")
+                                	  {
+					  
+				   mestatus.text('');
+				   $("#displayconceptimg").append('<tr id="conceptrow'+obj.insertid+'"><td><img src="'+obj.imagespath+'"  width="200px"></td><td id="conceptcol'+obj.insertid+'"><input class = "button2 pink white_text" type="button" value="Archive" onclick="concept_confirmation(1,'+$("#userroomid").val()+','+obj.insertid+');"/><input class = "button2 pink white_text" type="button" value="Delete &nbsp;" onclick="concept_confirmation(0,'+$("#userroomid").val()+','+obj.insertid+');"/></td><td>&nbsp;</td></tr>');		      
+		                    } 
+				  else
+				  {
+					mestatus.text('File upload failed!')
+				  }
+			
+				
+				
+			}
+		});
+		
+	});
+	
+	function concept_confirmation(conf,roomid,for_div)
+	{
+		$("#confirmation_div").remove();
+		var dismessage=(conf==1?"Sure?":"Sure?");
+
+	
+		$("#conceptcol"+for_div).append("<div class = 'white_text padding' style='position:absolute;z-index:100; background-color:lightgray;' id='confirmation_div'><div>"+dismessage+"</div><div><input class = 'button2 pink white_text' type='button' value='Yes' onclick='update_confirmation("+roomid+","+for_div+","+conf+");'/><input class = 'button2 white_text pink' type='button' value='No &nbsp;' onclick='remove_confirmation_box();'/></div></div>");
+		
+	}
+	
+        function remove_confirmation_box()
+        {
+	
+	      $("#confirmation_div").remove();
+	
+        }
+        function update_confirmation(roomid,conceptid,status)
+        {
+           $.post($("#siteurl").val()+"index.php/Admin/site/update_concept_board", {roomid :roomid,conceptid:conceptid,status:status}, function(data){
+           if(data.length>0)
+          {  
+		
+              $("#confirmation_div").remove();
+              if(status==0)
+              {
+              $("#conceptrow"+conceptid).remove();
+			   $("#conceptcomment"+conceptid).remove();
+              }
+          } 
+    }) 
+	        
+    }
+    
+  
+     
---
+++ assets/Scripts/user_validation.js
@@ -0,0 +1,290 @@
+
+$(document).ready(function()
+{
+   var ischack=false;
+   $("#radioyes").click(function()
+   {
+	   
+	$("#update_row_password").show();   
+	ischack=true;
+	}
+   )
+    $("#radiono").click(function()
+   {
+	   
+	$("#update_row_password").hide();   
+	   ischack=false;
+	}
+   )
+  $("#cancle_update").click(function()
+   {
+       $(".div_update_table").hide();
+  });
+  $("#update_update").click(function()
+   {
+	 
+        var e_value=1;
+        var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+        $("#div_show_error_message").html("");
+    if($("#update_name").val()=="")
+    {
+        $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Enter name:</p>'); 
+        e_value=0;
+    }
+    if($("#update_last_name").val().trim()=="")
+    {
+		 $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Enter last name:</p>'); 
+        e_value=0;
+	}
+	if($("#update_address").val().trim()=="")
+    {
+		 $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Enter address:</p>'); 
+        e_value=0;
+	}
+    if($("#update_email").val().trim()=="")
+    {
+        $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Enter email:</p>');
+        e_value=0;    
+    }
+   
+	 if(($("#update_phone").val().trim()==""))
+    {
+        $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Enter phone number:</p>');
+        e_value=0;    
+	}
+	if(($("#update_zip").val().trim()=="") )
+    {
+        $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Enter zip:</p>');
+        e_value=0;    
+	}
+	// if(($("#update_facebook").val()==""))
+    // {
+        // $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Enter Facebook:</p>');
+        // e_value=0;    
+	// }
+	// if(($("#update_pinterest").val()==""))
+    // {
+        // $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Enter Pinterest:</p>');
+        // e_value=0;    
+	// }
+if(($("#update_password").length>0))
+{
+		
+           if(($("#update_password").val()=="") && (ischack==true))
+           {
+           $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Enter password:</p>');
+           e_value=0;    
+           }
+	
+	  if(($("#update_password").val().trim()!="") && (ischack==true))
+	  {
+		
+            if($.trim($("#update_password").val()).length<4)
+            {
+		
+            $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*The User password field must be at least 4 characters in length:</p>');
+            e_value=0;    
+	    }
+	    if(($.trim($("#update_password").val()).length>32))
+	    {
+	      $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*The User password field can not exceed 32 characters in length:</p>');
+               e_value=0;	
+	   }
+           }
+ }
+    if(($("#update_email").val().trim()!="") && (!emailReg.test($("#update_email").val()))) 
+    {
+        $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Enter valid email address:</p>');
+        e_value=0;    
+    }
+    // if(($("#update_facebook").val().trim()!="") && (/^(http|https|ftp):\/\/[a-z0-9]+([-.]{1}[a-z0-9]+)*.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i.test($("#update_facebook").val()))==false)
+    // {
+       // $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Enter valid Url in facebook:</p>');
+       // e_value=0;
+	// }
+	// if(($("#update_pinterest").val().trim()!="") && (/^(http|https|ftp):\/\/[a-z0-9]+([-.]{1}[a-z0-9]+)*.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i.test($("#update_pinterest").val()))==false)
+    // {
+       // $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Enter valid Url in pinterest:</p>');
+       // e_value=0;
+	// }
+    if(e_value==1)
+    {
+
+		$("#radio_value").val(ischack);
+		$('#updateform').submit();
+				
+    }
+    else
+    {
+	  $("#div_show_error_message").focus();	
+		
+	}
+
+ });
+
+$("#update_room").click(function()
+{
+
+    var colorarea ="";
+	var area ="";
+ 
+    var file2=$("#update_photo2").val();
+    var fileextension_two = file2.substr( (file2.lastIndexOf('.') +1) );
+    var exts=new Array('jpg','png','gif','jpeg','pjpeg');
+   $("#div_show_error_message").html(''); 
+   e_value=1;
+  // if($("#update_budget").val().trim()=="")
+    // {
+     // $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Enter room budget:</p>'); 
+        // e_value=0;
+    // }
+    if($("#update_width").val().trim()=="")
+    {
+    $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Enter room width:</p>');
+    e_value=0; 
+    }
+   if($("#update_height").val().trim()=="")
+    {
+    $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Enter room height:</p>');
+    e_value=0;
+    }
+   
+    if($("#update_photo1").val().trim()!="")
+    {
+        var file1=$("#update_photo1").val();
+        var fileextension_one = file1.substr((file1.lastIndexOf('.') +1) );
+    if ($.inArray (fileextension_one.toLowerCase(), exts ) < 0 )
+        {
+         $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Enter room photo1 in correct format:</p>');
+         e_value=0;
+        } 
+    }
+    if($("#update_photo2").val().trim()!="")
+    {
+         var file2=$("#update_photo2").val();
+         
+         var fileextension_two = file2.substr((file2.lastIndexOf('.') +1) );
+         
+        if($.inArray (fileextension_two.toLowerCase(), exts ) < 0 )
+        {
+         $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Enter room photo2 in correct format:</p>');
+         e_value=0;
+        } 
+    }
+    if($("#update_height").val().trim()!="" && !$.isNumeric($("#update_height").val()))
+    {
+        $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Enter room height in numeric format:</p>');
+        e_value=0;
+    }
+    if($("#update_width").val().trim()!="" && !$.isNumeric($("#update_width").val()))
+    {
+        $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Enter room width in numeric format:</p>');
+        e_value=0;
+    }
+    // if($("#update_budget").val().trim()!="" && !$.isNumeric($("#update_budget").val()))
+    // {
+        // $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Enter budget in numeric format:</p>');
+        // e_value=0;
+    // }
+   if(e_value==1)
+   { 
+    
+    if($('input[name="color[]"]:checked').length<=0)
+	{
+	 $("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Select at lest one color pic:</p>');
+	 e_value=0;
+	}
+	else 
+	{
+		var colorarea ="";
+	   $('input[name="color[]"]:checked').each(function(i,e) {
+       if(colorarea=="")
+       {
+		colorarea=e.value;
+	   }
+	   else
+	   {
+		colorarea=colorarea+","+e.value;
+	   }
+	   
+    });	
+    
+    }
+
+	if($('input[name="style[]"]:checked').length<=0 && $("#select_room").is(':visible'))
+	{
+	$("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Select at lest one style pic:</p>');
+	e_value=0;
+	}else if($("#select_room").is(':visible'))
+	{
+	   var area ="";
+       $('input[name="style[]"]:checked').each(function(i,e) {
+       if(area=="")
+       {
+		area=e.value;
+	   }
+	   else
+	   {
+		area=area+","+e.value;
+	   }
+	
+      });		
+	}
+	
+	if($('input[name="optionstyle[]"]:checked').length<=0 && $("#option_room").is(':visible'))
+	{
+
+	$("#div_show_error_message").html($("#div_show_error_message").html()+'<p>*Select at lest one style pic:</p>');
+	e_value=0;
+	}else if($("#option_room").is(':visible'))
+	{
+		
+	   var area ="";
+       $('input[name="optionstyle[]"]:checked').each(function(i,e) {
+       
+       if(area=="")
+       {
+		area=e.value;
+	   }
+	   else
+	   {
+		area=area+","+e.value;
+	   }
+	   
+	   
+      });		
+	}
+
+if(e_value==1)
+{
+     $("#holdstyle").val(area);
+     $("#holdcolor").val(colorarea);
+    
+     $("#updateform").submit();
+ }
+   }
+}
+
+);
+$("#update_room_type").change(function() 
+{
+if($("#select_room").is(':visible'))
+{
+    $("#select_room").hide();
+    $("#option_room").show();
+}
+else 
+{
+	$("#select_room").show();
+	$("#option_room").hide();
+}
+});
+}); 
+function isValidUrl(url){
+    if(/^(http|https|ftp):\/\/[a-z0-9]+([-.]{1}[a-z0-9]+)*.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i.test(url)) {
+      return 1;
+    } else {
+      return -1;
+    }   
+}
+

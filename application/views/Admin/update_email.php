370e275148128531902baf7f773457640345f9f1
Ankit087 <ankit.kumar@kindlebit.com>
2013-09-28 02:19:33 +0530

    File Updated

---
+++ application/views/Admin/update_email.php
@@ -0,0 +1,50 @@
+<head>
+<link href='http://fonts.googleapis.com/css?family=Julius+Sans+One' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Sanchez' rel='stylesheet' type='text/css'>
+<style>
+.padding{
+padding: 3em;
+}
+
+.text{
+font-family: 'Sanchez';
+}
+.medium{
+font-size: 1em;
+}
+.midlarge{
+font-size: 1.3em;
+}
+.teal_text{
+color: #91C5C5;
+}
+.small{
+font-size: .5em;
+}
+.inline{
+display: inline-block;
+width: 30%;
+}
+ 
+</style>
+</head>
+<div class = "padding">
+<img class = "inline" src = "http://www.havenly.com/assets/Images/Blue_dalle.png" height=80>
+<br>
+<P class = "text dark_gray_text medium">
+Your room status has been changed by <?php echo $updateroom; ?>.
+</p>
+<p>
+<?php
+      echo  $message;
+ ?> 
+</p>
+Lee & Emily & the Havenly Team.
+
+<br><br><br><br>
+<p class = "small text">
+You are a member of Havenly.<br>
+Copyright © 2013 Havenly.<br>
+Havenly | 1062 Delaware St. | Denver, CO 80204
+</p>
